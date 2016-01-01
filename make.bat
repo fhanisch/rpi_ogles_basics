a95ff6fe3abff6dcb95c73a0e98b33979c3523f6
fh <felix.hanisch@live.de>
2016-01-01 16:03:22 +0100

    build-Verzeichnis eingeführt

--- make.bat
+++ make.bat
@@ -1,1 +1,6 @@
-gcc -o test main.c shader.c renderobject.c view.c matrix.c -lbcm_host -lEGL -lGLESv2 -I../../userland/userland-master/host_applications/linux/libs/bcm_host/include -I../../userland/userland-master -I../../userland/userland-master/build/inc/interface/vcos -L../../userland/userland-master/build/lib
+#kompilieren
+cd build
+gcc -c ../main.c ../shader.c ../renderobject.c ../view.c ../matrix.c ../ui/key.c -I/home/felix/Entwicklung/userland/userland-master/host_applications/linux/libs/bcm_host/include -I/home/felix/Entwicklung/userland/userland-master -I/home/felix/Entwicklung/userland/userland-master/build/inc/interface/vcos
+#linken
+gcc -o test main.o shader.o renderobject.o view.o matrix.o key.o -lbcm_host -lEGL -lGLESv2 -L/home/felix/Entwicklung/userland/userland-master/build/lib
+cd ..
