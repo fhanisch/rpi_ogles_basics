40ef5c14fb3fa78bde4ed09ef211eb48b47b6141
fh <felix.hanisch@live.de>
2016-01-24 16:28:08 +0100

    Z-Buffer initialisiert

--- main.c
+++ main.c
@@ -47,6 +47,7 @@ void initOpenGL(CUBE_STATE_T *p_state)
 	    EGL_GREEN_SIZE, 8,
 	    EGL_BLUE_SIZE, 8,
 	    EGL_ALPHA_SIZE, 8,
+	    EGL_DEPTH_SIZE, 16,
 	    EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
 	    EGL_NONE
 	};
